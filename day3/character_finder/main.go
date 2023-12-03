51351bab34e9a400742699259598e006b8d7592f
Nikola Medarevic <nikola@petmediagroup.com>
2023-12-03 22:28:02 +0100

    Day 3 - part 1 - find all special chars

---
+++ day3/character_finder/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"advent_helper/file_loader"
+	"fmt"
+	"regexp"
+)
+
+var escapedDot = regexp.QuoteMeta(".")
+var escapedNumerical = regexp.QuoteMeta("d")
+var specialCharacterPattern = "[^" + escapedDot + "\\d+]"
+var specialCharacterRegex = regexp.MustCompile(specialCharacterPattern)
+
+func main() {
+	var lines = file_loader.LoadLinesFromFile("../part1/input.txt")
+	var specialChars = make(map[string]string)
+
+	for _, line := range lines {
+		var allCharsInLine = specialCharacterRegex.FindAllString(line, -1)
+
+		for _, char := range allCharsInLine {
+			_, ok := specialChars[char]
+
+			if !ok {
+				specialChars[char] = char
+			}
+		}
+	}
+
+	for key, _ := range specialChars {
+		fmt.Println(key)
+	}
+}
+
+//* % @ - / + =
---
+++ day3/part1/main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	// var lines = file_loader.LoadLinesFromFile("./input.txt")
+
+}
