7041d0a2a159670f16c1000f813002ae230c8d4d
Nikola Medarevic <nikola@petmediagroup.com>
2023-12-06 20:49:41 +0100

    Day 3 - Part 2 Completed

--- day3/part2/main.go
+++ day3/part2/main.go
@@ -23,7 +23,7 @@ var numbersAdjecentToChars = []Number{}
 var numbersNearCogs = [][]Number{}
 
 var numberRegex = regexp.MustCompile(`\d+`)
-var starRegex = regexp.MustCompile(`\*`)
+var starRegex = regexp.MustCompile(`\*{1}`)
 
 func isSpecialCharacter(value byte) bool {
 	valueString := string(value)
@@ -52,11 +52,9 @@ func saveNumber(numberString string, lineIndex int, match []int, numberArray *[]
 		},
 		endPosition: Position{
 			vertical:   lineIndex,
-			horizontal: match[1],
+			horizontal: match[1] - 1,
 		},
 	})
-
-	fmt.Println("Added a number:", numberInt)
 }
 
 func getNumbersNearChars(lines []string, outputArray *[]Number) *[]Number {
@@ -64,8 +62,6 @@ func getNumbersNearChars(lines []string, outputArray *[]Number) *[]Number {
 
 	for lineIndex, line := range lines {
 		numberPositionMatches := numberRegex.FindAllStringSubmatchIndex(line, -1)
-		fmt.Println("-------------------------")
-		fmt.Println("Line", lineIndex)
 
 	continueLoop:
 		for _, match := range numberPositionMatches {
@@ -73,7 +69,6 @@ func getNumbersNearChars(lines []string, outputArray *[]Number) *[]Number {
 			if lineIndex > 0 {
 				// Check left diagonal up
 				if match[0] > 0 && isSpecialCharacter(lines[lineIndex-1][match[0]-1]) {
-					// fmt.Println(string(lines[lineIndex][match[0]-1]))
 					saveNumber(line[match[0]:match[1]], lineIndex, match, outputArray)
 					continue continueLoop
 				}
@@ -81,7 +76,6 @@ func getNumbersNearChars(lines []string, outputArray *[]Number) *[]Number {
 				// Check above between two numbers
 				for i := match[0]; i < match[1]; i++ {
 					if isSpecialCharacter(lines[lineIndex-1][i]) {
-						// fmt.Println(lines[lineIndex-1][i])
 						saveNumber(line[match[0]:match[1]], lineIndex, match, outputArray)
 						continue continueLoop
 					}
@@ -90,7 +84,6 @@ func getNumbersNearChars(lines []string, outputArray *[]Number) *[]Number {
 				// Check top diagonal up
 				// Only if the match is not on the last index
 				if match[1] < len(lines[lineIndex-1]) && isSpecialCharacter(lines[lineIndex-1][match[1]]) {
-					// fmt.Println(line[match[0]:match[1]], string(lines[lineIndex-1][match[1]]))
 					saveNumber(line[match[0]:match[1]], lineIndex, match, outputArray)
 					continue continueLoop
 				}
@@ -99,7 +92,6 @@ func getNumbersNearChars(lines []string, outputArray *[]Number) *[]Number {
 			// Check left
 			// If match is not on the first index
 			if match[0] > 0 && isSpecialCharacter(line[match[0]-1]) {
-				// fmt.Println(string(lines[lineIndex][match[0]-1]))
 				saveNumber(line[match[0]:match[1]], lineIndex, match, outputArray)
 				continue continueLoop
 			}
@@ -107,7 +99,6 @@ func getNumbersNearChars(lines []string, outputArray *[]Number) *[]Number {
 			// Check right
 			// If match is not on the last index
 			if match[1] < len(line) && isSpecialCharacter(line[match[1]]) {
-				// fmt.Println(string(lines[lineIndex][match[1]+1]))
 				saveNumber(line[match[0]:match[1]], lineIndex, match, outputArray)
 				continue continueLoop
 			}
@@ -117,7 +108,6 @@ func getNumbersNearChars(lines []string, outputArray *[]Number) *[]Number {
 				// Check left diagonal down
 				// Only if the match does not start at the first index
 				if match[0] > 0 && isSpecialCharacter(lines[lineIndex+1][match[0]-1]) {
-					// fmt.Println(string(lines[lineIndex+1][match[0]-1]))
 					saveNumber(line[match[0]:match[1]], lineIndex, match, outputArray)
 					continue continueLoop
 				}
@@ -126,7 +116,6 @@ func getNumbersNearChars(lines []string, outputArray *[]Number) *[]Number {
 				// Check above between two numbers
 				for i := match[0]; i < match[1]; i++ {
 					if isSpecialCharacter(lines[lineIndex+1][i]) {
-						// fmt.Println(string(lines[lineIndex+1][i]))
 						saveNumber(line[match[0]:match[1]], lineIndex, match, outputArray)
 						continue continueLoop
 					}
@@ -134,7 +123,6 @@ func getNumbersNearChars(lines []string, outputArray *[]Number) *[]Number {
 
 				// Check right diagonal dowm
 				if match[1] < len(lines[lineIndex+1]) && isSpecialCharacter(lines[lineIndex+1][match[1]]) {
-					// fmt.Println(string(lines[lineIndex+1][match[0]+1]))
 					saveNumber(line[match[0]:match[1]], lineIndex, match, outputArray)
 					continue continueLoop
 				}
@@ -161,23 +149,24 @@ func getNumber(vertical int, horizontal int, numbers *[]Number) *Number {
 }
 
 func findNumber(vertical int, horizontal int, inputArray *[]Number, outputArray *[][]Number) *Number {
-	if len(*outputArray) > 0 && len((*outputArray)[len(*outputArray)-1]) == 1 {
-		var alreadyFoundNumber = (*outputArray)[len(*outputArray)-1][0]
-
-		if alreadyFoundNumber.startPosition.vertical == vertical &&
-			alreadyFoundNumber.startPosition.horizontal <= horizontal &&
-			alreadyFoundNumber.endPosition.horizontal >= horizontal {
-			return nil
-		}
-	}
-
 	var number = getNumber(vertical, horizontal, inputArray)
 
 	return number
 }
 
 func addNumberToOutputArray(number *Number, outputArray *[][]Number) {
+	// Do not add existing numbers
+	for _, existingNumber := range (*outputArray)[len(*outputArray)-1] {
+		if existingNumber.startPosition.vertical == number.startPosition.vertical &&
+			existingNumber.startPosition.horizontal == number.startPosition.horizontal &&
+			existingNumber.endPosition.vertical == number.endPosition.vertical &&
+			existingNumber.endPosition.horizontal == number.endPosition.horizontal {
+			return
+		}
+	}
+
 	(*outputArray)[len(*outputArray)-1] = append((*outputArray)[len(*outputArray)-1], *number)
+	fmt.Print("Number ", number.number, "  ::::  ")
 }
 
 func findAllCogs(lines []string, numbersAdjecentToChars *[]Number, outputArray *[][]Number) *[][]Number {
@@ -186,54 +175,36 @@ func findAllCogs(lines []string, numbersAdjecentToChars *[]Number, outputArray *
 	for lineIndex, line := range lines {
 		cogMatches := starRegex.FindAllStringSubmatchIndex(line, -1)
 
-	continueLoop:
+		// continueLoop:
 		for _, match := range cogMatches {
 			*outputArray = append(*outputArray, []Number{})
-
+			fmt.Println("")
+			fmt.Println("Coords", lineIndex, match)
 			// If not the first line
 			if lineIndex > 0 {
 				// Check left diagonal up
 				if match[0] > 0 {
 					number := findNumber(lineIndex-1, match[0]-1, numbersAdjecentToChars, outputArray)
-					// var alreadyFoundNumber = (*outputArray)[len(*outputArray)-1][0]
-
-					// if alreadyFoundNumber.startPosition.vertical
-					// var number = getNumber(lineIndex-1, match[0]-1, numbersAdjecentToChars)
 
 					if number != nil {
 						addNumberToOutputArray(number, outputArray)
-						// (*outputArray)[len(*outputArray)-1] = append((*outputArray)[len(*outputArray)-1], *number)
-
-						if len(*outputArray) == 2 {
-							continue continueLoop
-						}
 					}
 				}
 
 				// Check above
-				for i := match[0]; i < match[1]; i++ {
-					number := findNumber(lineIndex-1, i, numbersAdjecentToChars, outputArray)
+				number := findNumber(lineIndex-1, match[0], numbersAdjecentToChars, outputArray)
 
-					if number != nil {
-						addNumberToOutputArray(number, outputArray)
-
-						if len(*outputArray) == 2 {
-							continue continueLoop
-						}
-					}
+				if number != nil {
+					addNumberToOutputArray(number, outputArray)
 				}
 
-				// Check top diagonal up
+				// Check right diagonal up
 				// Only if the match is not on the last index
 				if match[1] < len(lines[lineIndex-1]) {
-					number := findNumber(lineIndex-1, match[1], numbersAdjecentToChars, outputArray)
+					number := findNumber(lineIndex-1, match[0]+1, numbersAdjecentToChars, outputArray)
 
 					if number != nil {
 						addNumberToOutputArray(number, outputArray)
-
-						if len(*outputArray) == 2 {
-							continue continueLoop
-						}
 					}
 				}
 			}
@@ -245,26 +216,16 @@ func findAllCogs(lines []string, numbersAdjecentToChars *[]Number, outputArray *
 
 				if number != nil {
 					addNumberToOutputArray(number, outputArray)
-
-					if len(*outputArray) == 2 {
-						continue continueLoop
-					}
 				}
-				// fmt.Println(string(lines[lineIndex][match[0]-1]))
-				// saveNumber(line[match[0]:match[1]], lineIndex, match, outputArray)
 			}
 
 			// Check right
 			// If match is not on the last index
-			if match[1] < len(line) {
+			if match[0]+1 < len(line) {
 				number := findNumber(lineIndex, match[1], numbersAdjecentToChars, outputArray)
 
 				if number != nil {
 					addNumberToOutputArray(number, outputArray)
-
-					if len(*outputArray) == 2 {
-						continue continueLoop
-					}
 				}
 			}
 
@@ -277,25 +238,14 @@ func findAllCogs(lines []string, numbersAdjecentToChars *[]Number, outputArray *
 
 					if number != nil {
 						addNumberToOutputArray(number, outputArray)
-
-						if len(*outputArray) == 2 {
-							continue continueLoop
-						}
 					}
 				}
 
 				// Check below
-				// Check above between two numbers
-				for i := match[0]; i < match[1]; i++ {
-					number := findNumber(lineIndex+1, i, numbersAdjecentToChars, outputArray)
+				number := findNumber(lineIndex+1, match[0], numbersAdjecentToChars, outputArray)
 
-					if number != nil {
-						addNumberToOutputArray(number, outputArray)
-
-						if len(*outputArray) == 2 {
-							continue continueLoop
-						}
-					}
+				if number != nil {
+					addNumberToOutputArray(number, outputArray)
 				}
 
 				// Check right diagonal dowm
@@ -304,10 +254,6 @@ func findAllCogs(lines []string, numbersAdjecentToChars *[]Number, outputArray *
 
 					if number != nil {
 						addNumberToOutputArray(number, outputArray)
-
-						if len(*outputArray) == 2 {
-							continue continueLoop
-						}
 					}
 				}
 			}
@@ -329,23 +275,22 @@ func main() {
 	fmt.Println("Number of numbers:", len(numbersAdjecentToChars))
 	fmt.Println("Numbers around cogs:", len(numbersNearCogs))
 
-	for _, number := range numbersNearCogs {
-		fmt.Print(number[0].number)
-
-		if (len(number)) > 1 {
-			fmt.Print(" * ")
-			fmt.Print(number[1].number)
-		}
-
-		fmt.Println("=")
-	}
-
 	sum := 0
-	for _, number := range numbersAdjecentToChars {
-		sum += number.number
+	for _, number := range numbersNearCogs {
+		if (len(number)) == 2 {
+			sum += number[0].number * number[1].number
+		}
 	}
 
-	fmt.Println("Sum of all numbers adjecent to special characters:", sum)
+	if sum > 82818007 {
+		fmt.Println("Sum of all cog numbers products:", sum)
+	} else {
+		fmt.Println("Try again")
+	}
 }
 
-// Attempt 1 -
+// Attempt 1 - 80788281 x
+
+// 81035751 x - // Attempt 2 - to low
+
+// 82818007 - CORRECT
