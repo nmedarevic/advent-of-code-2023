5fed081a036b70ec717dee18539eecce91775070
Nikola Medarevic <nikola@petmediagroup.com>
2023-12-06 19:08:57 +0100

    Day 3 - Part 2 Continuation

--- day3/part2/main.go
+++ day3/part2/main.go
@@ -20,6 +20,7 @@ type Number struct {
 var specialCharsArray = []string{"#", "=", "/", "&", "%", "@", "$", "-", "*", "+"}
 
 var numbersAdjecentToChars = []Number{}
+var numbersNearCogs = [][]Number{}
 
 var numberRegex = regexp.MustCompile(`\d+`)
 var starRegex = regexp.MustCompile(`\*`)
@@ -144,14 +145,200 @@ func getNumbersNearChars(lines []string, outputArray *[]Number) *[]Number {
 	return outputArray
 }
 
+func getNumber(vertical int, horizontal int, numbers *[]Number) *Number {
+	for _, number := range *numbers {
+		// If numbers are not in the same line
+		if number.startPosition.vertical != vertical {
+			continue
+		}
+
+		if number.startPosition.horizontal <= horizontal && number.endPosition.horizontal >= horizontal {
+			return &number
+		}
+	}
+
+	return nil
+}
+
+func findNumber(vertical int, horizontal int, inputArray *[]Number, outputArray *[][]Number) *Number {
+	if len(*outputArray) > 0 && len((*outputArray)[len(*outputArray)-1]) == 1 {
+		var alreadyFoundNumber = (*outputArray)[len(*outputArray)-1][0]
+
+		if alreadyFoundNumber.startPosition.vertical == vertical &&
+			alreadyFoundNumber.startPosition.horizontal <= horizontal &&
+			alreadyFoundNumber.endPosition.horizontal >= horizontal {
+			return nil
+		}
+	}
+
+	var number = getNumber(vertical, horizontal, inputArray)
+
+	return number
+}
+
+func addNumberToOutputArray(number *Number, outputArray *[][]Number) {
+	(*outputArray)[len(*outputArray)-1] = append((*outputArray)[len(*outputArray)-1], *number)
+}
+
+func findAllCogs(lines []string, numbersAdjecentToChars *[]Number, outputArray *[][]Number) *[][]Number {
+	var numberOfLines = len(lines)
+
+	for lineIndex, line := range lines {
+		cogMatches := starRegex.FindAllStringSubmatchIndex(line, -1)
+
+	continueLoop:
+		for _, match := range cogMatches {
+			*outputArray = append(*outputArray, []Number{})
+
+			// If not the first line
+			if lineIndex > 0 {
+				// Check left diagonal up
+				if match[0] > 0 {
+					number := findNumber(lineIndex-1, match[0]-1, numbersAdjecentToChars, outputArray)
+					// var alreadyFoundNumber = (*outputArray)[len(*outputArray)-1][0]
+
+					// if alreadyFoundNumber.startPosition.vertical
+					// var number = getNumber(lineIndex-1, match[0]-1, numbersAdjecentToChars)
+
+					if number != nil {
+						addNumberToOutputArray(number, outputArray)
+						// (*outputArray)[len(*outputArray)-1] = append((*outputArray)[len(*outputArray)-1], *number)
+
+						if len(*outputArray) == 2 {
+							continue continueLoop
+						}
+					}
+				}
+
+				// Check above
+				for i := match[0]; i < match[1]; i++ {
+					number := findNumber(lineIndex-1, i, numbersAdjecentToChars, outputArray)
+
+					if number != nil {
+						addNumberToOutputArray(number, outputArray)
+
+						if len(*outputArray) == 2 {
+							continue continueLoop
+						}
+					}
+				}
+
+				// Check top diagonal up
+				// Only if the match is not on the last index
+				if match[1] < len(lines[lineIndex-1]) {
+					number := findNumber(lineIndex-1, match[1], numbersAdjecentToChars, outputArray)
+
+					if number != nil {
+						addNumberToOutputArray(number, outputArray)
+
+						if len(*outputArray) == 2 {
+							continue continueLoop
+						}
+					}
+				}
+			}
+
+			// Check left
+			// If match is not on the first index
+			if match[0] > 0 {
+				number := findNumber(lineIndex, match[0]-1, numbersAdjecentToChars, outputArray)
+
+				if number != nil {
+					addNumberToOutputArray(number, outputArray)
+
+					if len(*outputArray) == 2 {
+						continue continueLoop
+					}
+				}
+				// fmt.Println(string(lines[lineIndex][match[0]-1]))
+				// saveNumber(line[match[0]:match[1]], lineIndex, match, outputArray)
+			}
+
+			// Check right
+			// If match is not on the last index
+			if match[1] < len(line) {
+				number := findNumber(lineIndex, match[1], numbersAdjecentToChars, outputArray)
+
+				if number != nil {
+					addNumberToOutputArray(number, outputArray)
+
+					if len(*outputArray) == 2 {
+						continue continueLoop
+					}
+				}
+			}
+
+			// If not the last line
+			if lineIndex+1 <= numberOfLines-1 {
+				// Check left diagonal down
+				// Only if the match does not start at the first index
+				if match[0] > 0 {
+					number := findNumber(lineIndex+1, match[0]-1, numbersAdjecentToChars, outputArray)
+
+					if number != nil {
+						addNumberToOutputArray(number, outputArray)
+
+						if len(*outputArray) == 2 {
+							continue continueLoop
+						}
+					}
+				}
+
+				// Check below
+				// Check above between two numbers
+				for i := match[0]; i < match[1]; i++ {
+					number := findNumber(lineIndex+1, i, numbersAdjecentToChars, outputArray)
+
+					if number != nil {
+						addNumberToOutputArray(number, outputArray)
+
+						if len(*outputArray) == 2 {
+							continue continueLoop
+						}
+					}
+				}
+
+				// Check right diagonal dowm
+				if match[1] < len(lines[lineIndex+1]) {
+					number := findNumber(lineIndex+1, match[1], numbersAdjecentToChars, outputArray)
+
+					if number != nil {
+						addNumberToOutputArray(number, outputArray)
+
+						if len(*outputArray) == 2 {
+							continue continueLoop
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return outputArray
+}
+
 func main() {
 	var lines = file_loader.LoadLinesFromFile("./input.txt")
 
 	numbersAdjecentToChars = *getNumbersNearChars(lines, &numbersAdjecentToChars)
 
+	numbersNearCogs = *findAllCogs(lines, &numbersAdjecentToChars, &numbersNearCogs)
+
 	fmt.Println("-------------------------")
 
 	fmt.Println("Number of numbers:", len(numbersAdjecentToChars))
+	fmt.Println("Numbers around cogs:", len(numbersNearCogs))
+
+	for _, number := range numbersNearCogs {
+		fmt.Print(number[0].number)
+
+		if (len(number)) > 1 {
+			fmt.Print(" * ")
+			fmt.Print(number[1].number)
+		}
+
+		fmt.Println("=")
+	}
 
 	sum := 0
 	for _, number := range numbersAdjecentToChars {
@@ -161,6 +348,4 @@ func main() {
 	fmt.Println("Sum of all numbers adjecent to special characters:", sum)
 }
 
-// Attempt 2 = 474428 not correct
-// Attempt 3 - 520249 not correct
-// Attempt 4 - 539637 - correct!!!!
+// Attempt 1 -
